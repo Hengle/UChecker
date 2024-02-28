8715c7f83771f1d1cb2509fa847ec1f474385d14
chenxi.li <chenxi.li>
2024-02-28 17:04:42 +0800

    重构

---
+++ UCheckerPackageMaker/Assets/UChecker/Editor/Attribute/TreeViewAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UChecker.Editor
+{
+    [Serializable]
+    public class TreeViewAttribute: System.Attribute
+    {
+        public int priority;
+        public string name;
+        public TreeViewAttribute(string menuName,int priority = 0)
+        {
+            name = menuName;
+            this.priority = priority;
+        }
+    }
+}
